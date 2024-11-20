d83cf4c32ba2e2aa42f33e438887daade05ee751
isa-ocampo12 <isaocampo174@gmail.com>
2024-11-20 13:28:35 -0500

    Add files via upload

---
+++ simulador-seguros/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+export default function Header(){
+    return(
+        <header>
+            <img src="" alt="" />
+            <nav>
+                <a href="">Servicios</a>
+                <a href="">Contacto</a>
+            </nav>
+        </header>
+    )
+}
---
+++ simulador-seguros/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header"
+const Home = () => {
+    return (
+        <section>
+            <Header />
+            <main>
+                <h1>Contenido Principal</h1>
+            </main>
+        </section>
+    )
+}
+export default Home;
